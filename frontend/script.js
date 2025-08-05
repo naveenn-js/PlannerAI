dbac8b6cc70ed5f170521824f94ec245f7b0c958
naveenn-js <naveeneditsz@gmail.com>
2025-08-05 15:10:46 +0530

    Update API base URL to production

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://plannerai-2.onrender.com";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const modeToggleBtn = document.getElementById("mode-toggle");
